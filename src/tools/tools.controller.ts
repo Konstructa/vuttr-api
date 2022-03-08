59f195bda8aa3723b6c1fac9010d48cd2a649567
Konstructa <elenprojects7@gmail.com>
2022-03-07 23:53:31 -0300

    adicionado getall e create

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateToolDto } from './dto/create-tool.dto';
+import { ToolsService } from './tools.service';
 
 @Controller('tools')
-export class ToolsController {}
+export class ToolsController {
+  constructor(private readonly toolsService: ToolsService) {}
+
+  @Get()
+  getAll() {
+    return this.toolsService.getAll();
+  }
+
+  @Post()
+  create(@Body() createToolDto: CreateToolDto) {
+    return this.toolsService.create(createToolDto);
+  }
+}
--- src/tools/tools.service.ts
+++ src/tools/tools.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateToolDto } from './dto/create-tool.dto';
+import { Tool } from './entities/tool.entity';
 
 @Injectable()
-export class ToolsService {}
+export class ToolsService {
+  constructor(
+    @Inject('TOOLS_REPOSITORY')
+    private toolsRepository: Repository<Tool>,
+  ) {}
+
+  getAll() {
+    return this.toolsRepository.find();
+  }
+
+  async create(createToolDto: CreateToolDto) {
+    try {
+      const newTool = this.toolsRepository.create(createToolDto);
+      const result = await this.toolsRepository.save(newTool);
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+}
