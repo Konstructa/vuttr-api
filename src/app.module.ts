95c7105b3c023aebbc9b477fc3e8f84ad341c39e
Konstructa <elenprojects7@gmail.com>
2022-03-08 16:43:18 -0300

    add auth e users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { ToolsModule } from './tools/tools.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { toolsProviders } from './tools/entities/tools.providers';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ToolsModule,
     DatabaseModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [ToolsController],
   providers: [...toolsProviders, ToolsService],
