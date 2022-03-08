13283bcf198e189679b2193cd00764ec330bfee3
Konstructa <elenprojects7@gmail.com>
2022-03-08 16:59:23 -0300

    create findbyemail function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    { id: 1, email: 'admin@gmail.com', password: '1234' },
+    { id: 2, email: 'riribadgirl@gmail.com', password: 'diamonds' },
+  ];
+
+  findByEmail(email: string) {
+    const users = this.users.find((user) => user.email === email);
+    if (!users) return undefined;
+    return Promise.resolve(users);
+  }
+
+}
