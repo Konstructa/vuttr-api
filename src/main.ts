e45b4a7fd421a7bd8ddbcb10bece40a70bc6b32d
Konstructa <elenprojects7@gmail.com>
2022-03-09 18:25:24 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('VUTTR API - Milena')
+    .setDescription(
+      'A API VUTTR (Very Useful Tools to Remember) serve para registrar e encontrar ferramentas na área de TI que são extremamente úteis no nosso dia-a-dia',
+    )
+    .setVersion('1.0')
+    .addTag('tools')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
