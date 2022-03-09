1fa46414e56e3ebb4e6dbb18823cff8ae4f77c6c
Konstructa <elenprojects7@gmail.com>
2022-03-09 00:27:46 -0300

    add function retrierefreschtoken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { sign, verify } from 'jsonwebtoken';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import RefreshToken from './entities/refresh-token.entity';
@@ -9,6 +10,18 @@ export class AuthService {
 
   constructor(private readonly userService: UsersService) {}
 
+  private retrieveRefreshToke(refreshStr: string) {
+    try {
+      const decoded = verify(refreshStr, process.env.REFRESH_SECRET);
+      if (typeof decoded === 'string') return undefined;
+      return Promise.resolve(
+        this.refreshTokens.find((token) => token.id === decoded.id),
+      );
+    } catch {
+      return undefined;
+    }
+  }
+
   async login(
     email: string,
     password: string,
@@ -18,6 +31,8 @@ export class AuthService {
     if (!user) return undefined;
     //para hash
     if (user.password !== password) return undefined;
+
+    return this.newRefreshAndAccessToken(user, values);
   }
 
   private async newRefreshAndAccessToken(
@@ -33,5 +48,12 @@ export class AuthService {
       userId: user.id,
     });
     this.refreshTokens.push(refreshObject);
+
+    return {
+      refreshToken: refreshObject.sign(),
+      acessToken: sign({ userId: user.id }, process.env.ACCESS_SECRET, {
+        expiresIn: '1h',
+      }),
+    };
   }
 }
