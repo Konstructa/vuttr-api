b8290e0afb4b813b32a28b50084e338f80b30498
Konstructa <elenprojects7@gmail.com>
2022-03-09 23:23:24 -0300

    add verify user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Ip, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Ip,
+  Post,
+  Req,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ApiBody, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RefreshTokenDto } from './dto/refresh-token-dto';
@@ -8,11 +17,22 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna os erros pelo class-validator',
+  })
   async login(@Req() request, @Ip() ip: string, @Body() body: LoginDto) {
-    return this.authService.login(body.email, body.password, {
-      ipAdress: ip,
-      userAgent: request.headers['user-agent'],
-    });
+    try {
+      const check = await this.authService.login(body.email, body.password, {
+        ipAdress: ip,
+        userAgent: request.headers['user-agent'],
+      });
+
+      if (!check) throw new UnauthorizedException('Erro de senha ou email');
+    } catch {
+      return;
+    }
   }
 
   @Post('refresh')
