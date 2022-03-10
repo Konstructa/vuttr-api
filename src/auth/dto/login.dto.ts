68c791082c979387bf3f020e582c1b990dcc2a31
Konstructa <elenprojects7@gmail.com>
2022-03-10 00:30:55 -0300

    add swagger

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
+  @ApiProperty({
+    description: 'Este é email do usuário para fazer login',
+    example: 'mariabetania@gmail.com',
+  })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Este é a senha do usuário',
+    example: 'teamo',
+  })
   password: string;
 }
--- src/auth/dto/refresh-token-dto.ts
+++ src/auth/dto/refresh-token-dto.ts
@@ -1,6 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class RefreshTokenDto {
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Este é o refresh token gerado junto com o token',
+  })
   refreshToken: string;
 }
