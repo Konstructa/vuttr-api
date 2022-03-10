d326b63bd2bf6dc8aea785f55d87970d6aef4a83
Konstructa <elenprojects7@gmail.com>
2022-03-10 00:03:38 -0300

    add swagger api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,25 +22,46 @@ export class AuthController {
     status: 400,
     description: 'Retorna os erros pelo class-validator',
   })
+  @ApiResponse({
+    status: 401,
+    description: 'Erro de senha ou email',
+  })
   async login(@Req() request, @Ip() ip: string, @Body() body: LoginDto) {
     try {
-      const check = await this.authService.login(body.email, body.password, {
-        ipAdress: ip,
-        userAgent: request.headers['user-agent'],
-      });
+      const tokenAndRefreshToken = await this.authService.login(
+        body.email,
+        body.password,
+        {
+          ipAdress: ip,
+          userAgent: request.headers['user-agent'],
+        },
+      );
 
-      if (!check) throw new UnauthorizedException('Erro de senha ou email');
+      if (!tokenAndRefreshToken)
+        throw new UnauthorizedException('Erro de senha ou email');
+
+      return tokenAndRefreshToken;
     } catch {
       return;
     }
   }
 
   @Post('refresh')
+  @ApiBody({ type: RefreshTokenDto })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna os erros pelo class-validator',
+  })
   async refreshToken(@Body() body: RefreshTokenDto) {
     return this.authService.refresh(body.refreshToken);
   }
 
   @Delete('logout')
+  @ApiBody({ type: RefreshTokenDto })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna os erros pelo class-validator',
+  })
   async logout(@Body() body: RefreshTokenDto) {
     return this.authService.logout(body.refreshToken);
   }
