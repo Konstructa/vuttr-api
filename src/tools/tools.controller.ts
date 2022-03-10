2a82d9a8b5651e543b5d546bf9a1243021a6f279
Konstructa <elenprojects7@gmail.com>
2022-03-09 23:23:34 -0300

    add swagger

--- src/tools/tools.controller.ts
+++ src/tools/tools.controller.ts
@@ -8,6 +8,7 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateToolDto } from './dto/create-tool.dto';
 import { ToolsService } from './tools.service';
@@ -16,8 +17,23 @@ import { ToolsService } from './tools.service';
 export class ToolsController {
   constructor(private readonly toolsService: ToolsService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: 'Listar todas as ferramentas presentes no banco de dados',
+  })
+  @ApiResponse({ status: 401, description: 'Invalid JWT' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Mostra todas as ferramentas presentes ou encontradas pela tag',
+  })
+  @ApiResponse({ status: 404, description: 'Retorna uma [] vazia' })
+  @ApiQuery({
+    name: 'tag',
+    description: 'Tag pela qual será buscada as ferramentas',
+    required: false,
+  })
   getAll(@Query('tag') tag: string) {
     if (!tag) {
       return this.toolsService.getAll();
@@ -26,11 +42,24 @@ export class ToolsController {
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar uma ferramentar no banco de dados',
+  })
+  @ApiResponse({ status: 201, description: 'Retorna os dados criados com id' })
+  @ApiResponse({
+    status: 400,
+    description: 'Retorna os erros pelo class-validator',
+  })
+  @ApiBody({ type: CreateToolDto })
   create(@Body() createToolDto: CreateToolDto) {
     return this.toolsService.create(createToolDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Deletar uma ferramenta',
+  })
+  @ApiResponse({ status: 200, description: 'um objeto vazio {}' })
   delete(@Param('id') id: string) {
     return this.toolsService.delete(+id);
   }
