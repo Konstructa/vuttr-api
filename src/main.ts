3c2efff5f5b6f1f670616e5af89879bc2618e9aa
Milena <elenprojects7@gmail.com>
2022-03-20 21:13:44 -0300

    import dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
